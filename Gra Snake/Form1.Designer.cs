627af87efae7d371f5cedea206766fc214d9eadc
voykali <voy.kalinowski@gmail.com>
2016-12-08 10:19:51 +0100

    Add files via upload

--- Gra Snake/Form1.Designer.cs
+++ Gra Snake/Form1.Designer.cs
@@ -30,6 +30,11 @@
         {
             this.components = new System.ComponentModel.Container();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.snakePktLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.spacjaLabel = new System.Windows.Forms.Label();
+            this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // timer1
@@ -38,24 +43,65 @@
             this.timer1.Interval = 75;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.snakePktLabel});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 289);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(290, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(47, 17);
+            this.toolStripStatusLabel1.Text = "Punkty:";
+            // 
+            // snakePktLabel
+            // 
+            this.snakePktLabel.Name = "snakePktLabel";
+            this.snakePktLabel.Size = new System.Drawing.Size(13, 17);
+            this.snakePktLabel.Text = "0";
+            // 
+            // spacjaLabel
+            // 
+            this.spacjaLabel.AutoSize = true;
+            this.spacjaLabel.Location = new System.Drawing.Point(65, 131);
+            this.spacjaLabel.Name = "spacjaLabel";
+            this.spacjaLabel.Size = new System.Drawing.Size(128, 13);
+            this.spacjaLabel.TabIndex = 1;
+            this.spacjaLabel.Text = "Wciśnij Spację by zacząć";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(290, 289);
+            this.ClientSize = new System.Drawing.Size(290, 311);
+            this.Controls.Add(this.spacjaLabel);
+            this.Controls.Add(this.statusStrip1);
             this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Form1";
             this.Text = "Gra Snake";
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel snakePktLabel;
+        private System.Windows.Forms.Label spacjaLabel;
     }
 }
 
