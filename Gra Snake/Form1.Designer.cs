3c80a0c1154ae63b6aea464a3f98597078bc0d6b
voykali <voy.kalinowski@gmail.com>
2016-12-08 05:57:36 +0100

    Commit

--- Gra Snake/Form1.Designer.cs
+++ Gra Snake/Form1.Designer.cs
@@ -42,8 +42,9 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(290, 289);
             this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Form1";
             this.Text = "Gra Snake";
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
---
+++ Gra Snake/Jedzenie.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gra_Snake
+{
+    public class Jedzenie
+    {
+        private int x, y, szerokosc, wysoko;
+        private SolidBrush brush;
+        public Rectangle foodRec;
+
+    }
+}
