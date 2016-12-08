5541d8a540b4ee8e068cd0b299ab2908a77c1da9
voykali <voy.kalinowski@gmail.com>
2016-12-08 02:48:07 +0100

    rysowanie węża

--- Gra Snake/Snake.cs
+++ Gra Snake/Snake.cs
@@ -9,19 +9,31 @@ namespace Gra_Snake
 {
     public class Snake
     {
-        public Rectangle[] wazRect;
+        public Rectangle[] snakeRect;
         private SolidBrush brush;
         private int x, y, szerokosc, wysoko;
 
         public Snake()
         {
-            wazRect = new Rectangle[2];
+            snakeRect = new Rectangle[2];
             brush = new SolidBrush(Color.Green);
             x = 20;
             y = 0;
             szerokosc = 10;
             wysoko = 10;
 
+            for (int i = 0; i < snakeRect.Length; i++)
+            {
+                snakeRect[i] = new Rectangle(x, y, szerokosc, wysoko);
+                x -= 10;
+            }
+        }
+        public void rysujSnake(Graphics tlo)
+        {
+            foreach (Rectangle rec in snakeRect)
+            {
+                tlo.FillRectangle(brush, rec);
+            }
         }
     }
 }
