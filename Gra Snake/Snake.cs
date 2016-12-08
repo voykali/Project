a9dd2495624e3bcb5d03d2bb217a29b4a792f216
voykali <voy.kalinowski@gmail.com>
2016-12-08 02:27:04 +0100

    Add files via upload

--- Gra Snake/Snake.cs
+++ Gra Snake/Snake.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Gra_Snake
 {
-    class Snake
+    public class Snake
     {
+        public Rectangle[] wazRect;
+        private SolidBrush brush;
+        private int x, y, szerokosc, wysoko;
+
+        public Snake()
+        {
+            wazRect = new Rectangle[2];
+            brush = new SolidBrush(Color.Green);
+            x = 20;
+            y = 0;
+            szerokosc = 10;
+            wysoko = 10;
+
+        }
     }
 }
