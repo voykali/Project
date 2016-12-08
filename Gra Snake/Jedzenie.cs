ac4a49a75d6cb5f84dd1f49573bd3597c4d23258
voykali <voy.kalinowski@gmail.com>
2016-12-08 06:06:27 +0100

    Snake
    add  food

--- Gra Snake/Jedzenie.cs
+++ Gra Snake/Jedzenie.cs
@@ -9,9 +9,26 @@ namespace Gra_Snake
 {
     public class Jedzenie
     {
-        private int x, y, szerokosc, wysoko;
+        private int x, y, szeroko, wysoko;
         private SolidBrush brush;
         public Rectangle foodRec;
+        public Jedzenie(Random rndFood)
+        {
+            x = rndFood.Next(0, 29) * 10;
+            y = rndFood.Next(0, 29) * 10;
+            brush = new SolidBrush(Color.Red);
+            szeroko = 10;
+            wysoko = 10;
+            foodRec = new Rectangle(x, y, szeroko, wysoko);
+        }
+        public void polozenie(Random rndFood)
+        {
+            x = rndFood.Next(0, 29) * 10;
+            y = rndFood.Next(0, 29) * 10;
+        }
+        public void rysujJedzenie(Graphics tlo)
+        {
 
+        }
     }
 }
